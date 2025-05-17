95ccea84d68fb0833b98b1f3679faca9c06fdef5
oriol <filter.oriol@gmail.com>
2025-05-18 00:39:12 +0200

    Cleanup works

--- main.py
+++ main.py
@@ -7,16 +7,13 @@ import discord
 # from discord import Message
 
 from discord.ext.commands import Context
-# from discord.ext.commands import Bo
+from discord.ext import tasks
+from discord.ext.commands import Cog
 import os
 from urlextract import URLExtract
 from urllib.parse import urlparse
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
+import asyncio
+from datetime import timezone
 
 @dataclasses.dataclass
 class MessageRecord:
@@ -99,60 +96,97 @@ class MessagesDB:
                 url=url
             )
 
+@dataclasses.dataclass
+class Config:
+    messages_db: MessagesDB
+    # thresholds_seconds: int
+    # count_threshold: 5
 
-messages_db = MessagesDB()
+class MyBot(discord.Client):
+    lock = asyncio.Lock
+
+    def __init__(self, intents: discord.Intents = None):
+        intents.message_content = True
+        super().__init__(intents=intents)
+
+        self.config = Config(
+            messages_db=MessagesDB()
+        )
+
+        self.lock = asyncio.Lock()
+
+    # @discord.ext. event
+    async def on_ready(self):
+        print(f'We have logged in as {client.user}')
+        self.messages_cleanup.start()
+
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            pass  # Ignore bots
+        elif not message.guild.id:
+            pass  # Ignore messages without guild id
+        else:
+            extractor = URLExtract()
+
+            urlextract = extractor.find_urls(message.content)
+            sanitized_urls = [f"{urlparse(link).netloc}/{urlparse(link).path}" for link in urlextract]
+            # sanitized_url
+            if len(sanitized_urls) > 0:
+                message_object = MessageRecord(
+                    id=message.id,
+                    author_id=message.author.id,
+                    server_id=message.guild.id,
+                    creation_timestamp=message.created_at,
+                    links=sanitized_urls
+                )
+                self.config.messages_db.add_message(message_object)
+
+                for url in sanitized_urls:
+                    count = self.config.messages_db.count_links_from_author(
+                        server_id=message_object.server_id,
+                        author_id=message_object.author_id,
+                        timestamp=message_object.creation_timestamp,
+                        url=url)
+                    print(f"Count {count} for URL '{url}'")
+
+            else:
+                pass
+                # Ignore cases where no links are provided.
+
+                # Check threshold
+                # If above the threshold
+                # 1. Tiemout  # Prevent
+                # 2. Ping admins  # Request confirmation
+                # 3. Remove recent messages  # Embed in the admins channel for admins to decide what to do with it?
+
+    # @tasks.loop(seconds=1.0, count=5)
+    # async def slow_count(self):
+    #     print("hi")
+    @tasks.loop(seconds= 5)
+    async def messages_cleanup(self):
+        async with self.lock:
+            print("start job")
+            global thresholds_seconds
+            job_start_timestamp = datetime.datetime.now(datetime.UTC)
+            bottom_threshold = job_start_timestamp - datetime.timedelta(seconds=thresholds_seconds+5) # Adding an extra margin just in case
+            for server in self.config.messages_db.servers.values():
+                server: MessagesDBServer
+                for author in server.authors.values():
+                    author: MessagesDBServerAuthor
+                    for message in author.messages:
+                        message: MessageRecord
+                        if message.creation_timestamp < bottom_threshold:
+                            print(f"Deleting message {message.id} from user {message.author_id} (serger {message.server_id})")
+                            author.messages.remove(message)
+
+
 thresholds_seconds = 7
 count_threshold = 5
 
+if __name__ == '__main__':
+    _intents = discord.Intents.default()
+    _intents.message_content = True
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-
-@client.event
-async def on_message(message: discord.Message):
-    if message.author.bot:
-        pass  # Ignore bots
-    elif not message.guild.id:
-        pass  # Ignore messages without guild id
-    else:
-        extractor = URLExtract()
-
-        urlextract = extractor.find_urls(message.content)
-        sanitized_urls = [f"{urlparse(link).netloc}/{urlparse(link).path}" for link in urlextract]
-        # sanitized_url
-        if len(sanitized_urls) > 0:
-            message_object = MessageRecord(
-                id=message.id,
-                author_id=message.author.id,
-                server_id=message.guild.id,
-                creation_timestamp=message.created_at,
-                links=sanitized_urls
-            )
-            global messages_db
-            messages_db.add_message(message_object)
-
-            for url in sanitized_urls:
-                count = messages_db.count_links_from_author(
-                    server_id=message_object.server_id,
-                    author_id=message_object.author_id,
-                    timestamp=message_object.creation_timestamp,
-                    url=url)
-                print(f"Count {count} for URL '{url}'")
-
-        else:
-            pass
-            # Ignore cases where no links are provided.
-
-            # Check threshold
-            # If above the threshold
-            # 1. Tiemout  # Prevent
-            # 2. Ping admins  # Request confirmation
-            # 3. Remove recent messages  # Embed in the admins channel for admins to decide what to do with it?
-
-    # print(message)
-
-
-token = os.getenv("API_TOKEN")
-client.run(token)
+    token = os.getenv("API_TOKEN")
+    client = MyBot(_intents)
+    client.run(token)
